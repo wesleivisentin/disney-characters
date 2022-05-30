40ea9ad6ec8b7b80a5fb165e5eb6bc78a8a00f47
weslei <wesleivisentin@hotmail.com>
2022-05-29 21:33:13 -0300

    5

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,28 @@ personagens.forEach((personagem) => {
     personagem.addEventListener("mouseenter", () => {
 
         const idSelecionado = personagem.attributes.id.value
+        console.log(personagem);
        
        
         if(idSelecionado === "buzz-lightyear") {
             let imagemJogador = document.getElementById("personagem-jogador-1")
             imagemJogador.src = `src/imagens/toy/${idSelecionado}2.png`
+
+            const nomeJogador11 = document.getElementById("nome-jogador-1")
+         const nomeSelecionadoo = personagem.getAttribute("data-name")
+         nomeJogador11.innerHTML = nomeSelecionadoo
             return
+        }
+         else if(idSelecionado === "jasmine") {
+            let imagemJogador = document.getElementById("personagem-jogador-11")
+            imagemJogador.src = `src/imagens/alladin/${idSelecionado}2.png`
+            
+            
+            const nomeJogador11 = document.getElementById("nome-jogador-11")
+         const nomeSelecionadoo = personagem.getAttribute("data-name")
+         nomeJogador11.innerHTML = nomeSelecionadoo
+            return
+
         }
 
 
@@ -32,9 +48,20 @@ personagens.forEach((personagem) => {
          nomeJogador1.innerHTML = nomeSelecionado
 
 
+
+        
+        const imagemJogador11 = document.getElementById("personagem-jogador-11")
+        imagemJogador11.src = `src/imagens/alladin/${idSelecionado}.png`
+
+         const nomeJogador11 = document.getElementById("nome-jogador-11")
+         const nomeSelecionadoo = personagem.getAttribute("data-name")
+         nomeJogador11.innerHTML = nomeSelecionadoo
+
+
     })
 
 })
 
 
 
+//troca de desenho
