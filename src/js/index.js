d17a93d3dce59740dcb5be20343603eaeca280d2
weslei <wesleivisentin@hotmail.com>
2022-06-10 19:22:33 -0300

    2

--- src/js/index.js
+++ src/js/index.js
@@ -7,24 +7,35 @@ personagens.forEach((personagem) => {
         const idSelecionado = personagem.attributes.id.value
         
 
-        function nomeP1(call,call2){
-            let nomeJogador1 = document.getElementById(call)
-            let nomeSelecionadoo = personagem.getAttribute("data-name")
-            nomeJogador1.innerHTML = nomeSelecionadoo
+        
 
-            let imagemJogador = document.getElementById(call)
-            imagemJogador.src = `src/imagens/${call2}/${idSelecionado}2.png`
+        function imagemP1(cal, cal2, cal3) {
+            let imagemJogador = document.getElementById(cal)
+            imagemJogador.src = `src/imagens/${cal2}/${cal3}2.png`
+            
         }
-      
+
+        
        
         if(idSelecionado === "buzz-lightyear") {
            
-            nomeP1("personagem-jogador-1", "toy")
+        
+            imagemP1("personagem-jogador-1","toy", idSelecionado)
+            let nomeJogador1 = document.getElementById("nome-jogador-1")
+            let nomeSelecionado = personagem.getAttribute("data-name")
+            nomeJogador1.innerHTML = nomeSelecionado
+           
             return
         }
+
+
          else if(idSelecionado === "jasmine") {
         
-            nomeP1("personagem-jogador-11", "alladin")
+            imagemP1("personagem-jogador-11","alladin", idSelecionado)
+            let nomeJogador11 = document.getElementById("nome-jogador-11")
+            let nomeSelecionadoo = personagem.getAttribute("data-name")
+            nomeJogador11.innerHTML = nomeSelecionadoo
+            
             return
 
         }
@@ -64,14 +75,66 @@ personagens.forEach((personagem) => {
 
 
 
-//troca de desenho
+// troca de desenho
 
 
 const cenarios = document.querySelectorAll(".desenho")
+const idCenario = document.querySelectorAll(".selecao-de-personagens")
 
    cenarios.forEach((cenario)=> {
 
         cenario.addEventListener("click", ()=> {
 
+            //remove o desenho aberto
+            const desenhoAberto = document.querySelector(".principal-selecionado")
+            
+            desenhoAberto.classList.remove("principal-selecionado")
+
+
+
+
+
+            const idSelecionado = cenario.attributes.id.value
+        //     console.log(idSelecionado);
+
+            const idCenarioParaMostrar = "cenario-" + idSelecionado
+            const cenarioParaMostrar = document.getElementById(idCenarioParaMostrar)
+            cenarioParaMostrar.classList.add("principal-selecionado")
+
+
+
+
+            const cenarioSelecionado = document.querySelector(".selected")
+        
+        cenarioSelecionado.classList.remove("selected")
+
+            const cenarioAtivo = document.getElementById(idSelecionado)
+            cenarioAtivo.classList.add("selected")
+
+
+
+
+        // cenario.classList.add("selected")
+
+        // //////////////////
+        
+
+         
+        //  idCenario.attributes.id.value
+        //  console.log(idCenario)
+                
+            
+
+
+         
+        
+            
+
+
         })
    })
+
+
+//    const cenarioSelecionadoPorJogador = document.querySelector(".principal-selecionado")
+//         cenarioSelecionadoPorJogador.classList.remove("principal-selecionado")
+//         cenario.classList.add("principal-selecionado")
