e65ef01d1c1d81a0225df3174d30e5e2c06ae184
weslei <wesleivisentin@hotmail.com>
2022-06-18 20:24:19 -0300

    musicas cenarios adicionados

--- src/js/music.js
+++ src/js/music.js
@@ -1,21 +1,19 @@
 
-let audios = [
-    {caminho:'audios/grito.mp3', legenda:'GRITO'},
-    {caminho:'audios/jesus.mp3', legenda:'Jesus'},
-    {caminho:'audios/nao-e-o-pai.mp3', legenda:'Ele não é o pai'},
-    {caminho:'audios/pare.mp3', legenda:'PARE'},
-    {caminho:'audios/que-isso-meu-filho.mp3', legenda:'Que isso meu fio'},
-    {caminho:'audios/ratinhoo.mp3', legenda:'RATINHO'},
-    {caminho:'audios/uepa.mp3', legenda:'UEPA'},
-    {caminho:'audios/vixi-ratinho.mp3', legenda:'VIXI'},
-    {caminho:'audios/xaropinho.mp3', legenda:'Xaropinho'},
+let musicas = [
+    {caminho:'src/music/toy-story-theme.mp3'},
+    {caminho:'src/music/aladdin.mp3'},
+    {caminho:'src/music/os-incriveis.mp3'},
+    {caminho:'src/music/mickey.mp3'},
+    {caminho:'src/music/princess.mp3'},
+    {caminho:'src/music/villains.mp3'},
+    
 ];
 
-let botoes = document.querySelectorAll('.botao');
-let legendas = document.querySelectorAll('p');
+let botoes = document.querySelectorAll('.desenho');
 
-for (let i=0; i < 9; i++){
-    legendas[i].textContent = audios[i].legenda;
+
+for (let i=0; i < 6; i++){
+    
     botoes[i].setAttribute('data-item', i);
 }
 
@@ -23,7 +21,7 @@ let audioTag = document.querySelector('audio');
 
 botoes.forEach(botao => {
     botao.addEventListener('click', () => {
-        let som = audios[botao.getAttribute('data-item')];
+        let som = musicas[botao.getAttribute('data-item')];
         audioTag.setAttribute('src', som.caminho);
         
         audioTag.addEventListener('loadeddata', () => {
@@ -31,3 +29,4 @@ botoes.forEach(botao => {
         });
     });
 });
+
